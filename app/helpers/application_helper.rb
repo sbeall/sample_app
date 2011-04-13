e80fe8aace4eb845565458b047b9466cce1a5188
unknown <SCB@.(none)>
2011-04-13 10:19:34 -0400

    Removed default rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returns the correct title to the page
+  def title
+    base_title = "Tutorial"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
